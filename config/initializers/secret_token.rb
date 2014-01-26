f0e955211c44b18f99060bf65ab536162b1fdcf7
Bartlomiej Skwira <bartlomiej.skwira@amberbit.com>
2014-01-26 12:09:14 +0100

    Create new Rails app - Gadget Collection

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GadgetCollection::Application.config.secret_key_base = 'a13ab5422ae5b54b44141ac9d023dbb4e6eb687a6dae4ff30e95cbd01366ffc29dd071033b42e2a490227271fba2d30dbd291dee8aad55c3651d83b6157dd7ff'
