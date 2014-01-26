1fdd667f3ade04f3b2f92c37d7caf08d44bfbc54
Bartlomiej Skwira <bartlomiej.skwira@amberbit.com>
2014-01-26 13:23:54 +0100

    basic login/logout tests

---
+++ spec/features/sessions_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../spec_helper'
+
+
+feature "User sessions" do
+  background do
+    @user = create :user
+  end
+
+  scenario "Signing in with correct credentials" do
+    visit root_path
+    click_link 'Login'
+    within("#session") do
+      fill_in 'Login', :with => 'user@example.com'
+      fill_in 'Password', :with => 'applift'
+    end
+    click_link 'Sign in'
+    expect(page).to have_content 'Signed in successfully'
+  end
+
+  scenario "Signing out with correct credentials" do
+    visit new_session_path
+    within("#session") do
+      fill_in 'Login', :with => 'user@example.com'
+      fill_in 'Password', :with => 'applift'
+    end
+    click_link 'Sign in'
+    click_link 'Sign out'
+    expect(page).to have_content 'Signed out successfully'
+  end
+
+end
