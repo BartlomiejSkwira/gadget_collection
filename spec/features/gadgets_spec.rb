d2d8c7cfe3660b3e5872c9e887bf7e7497b89bec
Bartlomiej Skwira <bartlomiej.skwira@amberbit.com>
2014-01-27 19:56:49 +0100

    gadgets and profile links in menu

--- spec/features/gadgets_spec.rb
+++ spec/features/gadgets_spec.rb
@@ -9,7 +9,7 @@ I can' do
     @user = create :user
     3.times { create(:gadget, user: @user) }
     sign_in @user
-    click 'Gadgets'
+    click_link 'Gadgets'
   end
 
   scenario 'see a list of gadgets' do
