b1b4a45b95545818dc236f34126d4122b1242df6
Bartlomiej Skwira <bartlomiej.skwira@amberbit.com>
2014-01-27 18:19:39 +0100

    remove redundant code

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ActiveRecord::Base
     SecureRandom.urlsafe_base64
   end
 
-  def User.encrypt(token)
-    Digest::SHA1.hexdigest(token.to_s)
-  end
-
   private
 
     def create_remember_token
