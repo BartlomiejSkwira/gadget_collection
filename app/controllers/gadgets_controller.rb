d54800b287d837e21115666ab866d036fce979a5
Bartlomiej Skwira <bartlomiej.skwira@amberbit.com>
2014-01-27 19:47:13 +0100

    gadgets controller

---
+++ app/controllers/gadgets_controller.rb
@@ -0,0 +1,10 @@
+class GadgetsController < ApplicationController
+
+  def index
+    if params[:user_id]
+      @user = User.includes(:gadgets).find_by_id(params[:user_id])
+    else
+      redirect_to root_path
+    end
+  end
+end
