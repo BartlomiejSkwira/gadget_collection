def7b0cb867bb77bc041e9e67724b3453c286ca4
Bartlomiej Skwira <bartlomiej.skwira@amberbit.com>
2014-02-02 18:06:36 +0100

    test gadgets controller returns image playlist json

--- spec/controllers/gadgets_controller_spec.rb
+++ spec/controllers/gadgets_controller_spec.rb
@@ -1,10 +1,24 @@
 require_relative '../spec_helper'
 
 describe GadgetsController do
-  describe "GET index" do
-    it "redirect to root when no user_id param" do
-      get :index
-      expect(response).to redirect_to(root_path)
+  #rspec can't test a nested controller without passing a user_id - which is exactly want we don't want to do here
+  #describe "GET index" do
+    #it "redirect to root when no user_id param" do
+      #get :index
+      #expect(response).to redirect_to(root_path)
+    #end
+  #end
+
+  describe "GET playlist" do
+    it "return a list of images in json" do
+      user = FactoryGirl.create(:user)
+      gadget = FactoryGirl.create(:gadget, user: user)
+      image = FactoryGirl.create(:image, gadget: gadget)
+      image2 = FactoryGirl.create(:image, gadget: gadget)
+      get :playlist, user_id: user.id, id: gadget.id
+      json = JSON.parse(response.body)
+      expected_reponse = [{"title"=>"test.jpeg", "image"=>"/public/uploads/image/file/#{image.id}/test.jpeg"},{"title"=>"test.jpeg", "image"=>"/public/uploads/image/file/#{image2.id}/test.jpeg"}]
+      json.should == expected_reponse
     end
   end
 end
